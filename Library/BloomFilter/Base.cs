78469d6105a06c979d80c20531f4d63c80610048
grzegorz <g.goreczny@gmail.com>
2018-08-19 21:12:07 +0100

    Bloom filter init

---
+++ Library/BloomFilter/Base.cs
@@ -0,0 +1,33 @@
+﻿namespace Library.BloomFilter
+{
+    class Base
+    {
+        private bool[] _filter;
+
+        public Base()
+        {
+            _filter = new bool[10];
+        }
+
+
+        public void  Add(string value)
+        {
+            var position = getHash(value);
+            _filter[position] = true;
+        }
+
+        public bool HasValue(string value)
+        {
+            var position = getHash(value);
+            return _filter[position];
+        }
+
+
+        private int getHash(string value)
+        {
+            return 0;
+        }
+
+        
+    }
+}
