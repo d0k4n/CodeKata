1bb4eece2d15a4f11f71927a20eaf7d7cc886cfb
grzesiek <g.goreczny@gmail.com>
2018-10-01 23:48:47 +0100

    Create murmur3 hash

---
+++ Library/BloomFilter/MurmurHash3.cs
@@ -0,0 +1,33 @@
+﻿namespace Library.BloomFilter
+{
+    using System;
+
+    public class MurmurHash3
+    {
+        public string Hash(string key)
+        {
+
+            uint seed = 0;
+
+            uint c1 = 0xcc9e2d51;
+            uint c2 = 0x1b873593;
+            var r1 = 15;
+            var r2 = 13;
+            var m = 5;
+            uint n = 0xe6546b64;
+
+
+
+
+            return string.Empty;
+        }
+
+         public static UInt64 RotateRight(UInt64 x, int n) {
+            return (((x) >> (n)) | ((x) << (64-(n))));
+        }
+
+        public static UInt64 RotateLeft(UInt64 x, int n) {
+            return (((x) << (n)) | ((x) >> (64-(n))));
+        }
+    }
+}
---
+++ LibraryTest/MurmurHash3Test.cs
@@ -0,0 +1,26 @@
+﻿namespace LibraryTest
+{
+
+    using Library.BloomFilter;
+    using NUnit.Framework;
+    
+    [TestFixture]
+    public class MurmurHash3Test
+    {
+
+        //Murmur (Ver 3) - 128-bit - key of Zero - four byte long of 0 value
+
+        [TestCase("Test1", "c2a3056c5b5f012b1b4689af945f0117") ]
+        [TestCase("github", "4c4462c42dc86da0ecac78d742855563") ]
+        [TestCase("protrip", "4e288d17bc05bb014a5a0e359c432183") ]
+        [TestCase("NoNotReallyNo", "a068b629d263732e2bf4c1157b8e43d7") ]
+        public void Given_Key_When_Using_Murmur3_Then_Got_Expected_Result(string key, string expectedResult)
+        {
+            var murmur3 = new MurmurHash3();
+            var hash = murmur3.Hash(key);
+
+            Assert.That(hash, Is.EqualTo(expectedResult));
+        }
+
+    }
+}
